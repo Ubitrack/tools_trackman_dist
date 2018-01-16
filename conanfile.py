28d497df4194156bf85e569ff93166b0d42fb273
Ulrich Eck <ueck@net-labs.de>
2018-01-16 21:11:28 +0100

    added initial draft of conan build spec

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile
+
+class TrackmanConan(ConanFile):
+    name = "ubitrack_tools_trackman"
+    version = "1.0"
+    build_policy = "missing"
+    no_copy_source = True
+
+    requires = (
+        "java_installer/9.0.0@bincrafters/stable",
+        "ubitrack_facade/[>=1.3.0]@ubitrack/stable",
+        )
+
+    def build(self):
+      pass
+
+    def package(self):
+        self.copy("*", dst="", keep_path=True)
+
+    def package_info(self):  
+        self.output.info("Added Trackman Editor v%s" % self.version)
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.TRACKMAN_LIB_PATH.append(os.path.join(self.package_folder, "lib"))
